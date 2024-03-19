9e5b254bd417d1b8dd640336d2a9ff45598a622e
mjeongriver <jennygh2@hanmail.net>
2024-03-20 08:37:08 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,21 @@ import hello.core.order.OrderServiceImpl;
 
 //애플리케이션의 실제 동작에 필요한 구현 객체를 생성 한다.
 //객체의 생성과 연결은 'AppConfig'가 담당한다.
+//역할과 구현을 분리하여 리팩토링 -> 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악 가능
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
