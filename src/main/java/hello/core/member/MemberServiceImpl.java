ebde474d5596c2c6374b5cd98e47f51d8f73dddd
mjeongriver <jennygh2@hanmail.net>
2024-03-19 16:34:59 +0900

    관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,15 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    //생성자 주입
+    //클라이언트인 memberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서 DI, 우리말로 의존관계 주입이라 한다.
+    //생성자를 통해 어떤 구현 객체가 들어올지는 알 수 없다. 실행에만 집중!
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
