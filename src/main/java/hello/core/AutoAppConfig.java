d004ec422a605127700265b00888c008e1c99db8
mjeongriver <jennygh2@hanmail.net>
2024-03-26 16:32:55 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //Configuration을 제외하기 위한 설정
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
@@ -7,6 +11,7 @@ public class MemberServiceImpl implements MemberService{
     //생성자 주입
     //클라이언트인 memberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서 DI, 우리말로 의존관계 주입이라 한다.
     //생성자를 통해 어떤 구현 객체가 들어올지는 알 수 없다. 실행에만 집중!
+    @Autowired //의존관계 설정, ac.getBean(MemberRepository.class)와 동일하다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
